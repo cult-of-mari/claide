561a6a7f33c8ba039f7fe4cab5606042fb9b5dce
mizz1e <158266562+mizz1e@users.noreply.github.com>
2024-07-17 21:46:00 +0800

    dont use python

--- src/main.rs
+++ src/main.rs
@@ -1,19 +1,25 @@
 use {
-    pyo3::{conversion, prelude::*, types::PyDict, PyTypeCheck},
-    python::langchain::LangGraph,
+    self::util::{StrExt, UserExt},
+    futures::future,
+    reqwest::Client,
+    serde::{Deserialize, Serialize},
     serenity::{
-        all::{
-            ActivityData, Channel, Client, Context, CreateMessage, GatewayIntents, GetMessages,
-            Message, OnlineStatus, Settings,
+        all::Mentionable,
+        builder::CreateMessage,
+        cache::Settings,
+        client::{ClientBuilder, Context, EventHandler},
+        gateway::ActivityData,
+        http::HttpBuilder,
+        model::{
+            channel::{Message, MessageType},
+            gateway::GatewayIntents,
+            user::OnlineStatus,
         },
-        prelude::*,
+        prelude::TypeMapKey,
     },
-    std::{env, process, sync::Arc, time::Instant},
-    time::Duration,
-    tokio::{signal, task},
+    std::{env, iter, time::Duration},
 };
 
-pub mod python;
 pub mod util;
 
 struct Handler;
@@ -21,158 +27,367 @@ struct Handler;
 #[serenity::async_trait]
 impl EventHandler for Handler {
     async fn message(&self, context: Context, message: Message) {
-        handle_message(context, message).await.ok();
+        let _result = tokio::spawn(handle_message(context, message)).await;
     }
+}
+
+pub struct AccountKey;
+
+#[derive(Clone, Debug)]
+pub struct Account {
+    token: String,
+    intents: GatewayIntents,
+    client: Client,
+    activity: String,
+    status: OnlineStatus,
+    name: String,
+    personality: String,
+    footer: bool,
+}
+
+impl TypeMapKey for AccountKey {
+    type Value = Account;
+}
+
+#[derive(Clone, Copy, Debug, Deserialize, Eq, PartialEq, Serialize)]
+#[serde(rename_all = "snake_case")]
+pub enum Role {
+    Assistant,
+    System,
+    User,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ConversationMessage {
+    role: Role,
+    content: String,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct Conversation {
+    model: String,
+    messages: Vec<ConversationMessage>,
+    stream: bool,
+}
+
+#[derive(Clone, Debug, Deserialize, Serialize)]
+pub struct ConversationResult {
+    eval_count: u64,
+    eval_duration: u64,
+    message: ConversationMessage,
 }
 
 async fn handle_message(context: Context, message: Message) -> anyhow::Result<()> {
-    // Ignore messages from yourself.
-    if message.author.id == context.cache.current_user().id {
+    if !matches!(
+        message.kind,
+        MessageType::Regular | MessageType::InlineReply
+    ) {
         return Ok(());
     }
 
-    if message.channel_id != 1261932165453582348 {
+    let current_user_id = context.cache.current_user().id;
+
+    if message.author.id == current_user_id {
         return Ok(());
     }
 
-    let channel = message.channel(&context).await?;
-    let get_messages = GetMessages::new().before(message.id).limit(25);
-    let messages = match channel {
-        Channel::Guild(channel) => channel.messages(&context, get_messages).await?,
-        Channel::Private(channel) => channel.messages(&context, get_messages).await?,
-        _ => return Ok(()),
-    };
+    let is_mentioned = message
+        .mentions
+        .iter()
+        .any(|user| user.id == current_user_id);
 
-    let name = "Clyde";
-    let location = "Server";
-    let users = "mari (kalmari246):";
+    let is_replied_to = message
+        .referenced_message
+        .as_ref()
+        .is_some_and(|message| message.author.id == current_user_id);
 
-    // rustfmt wants the string on its own lines, and that looks horrid.
-    #[rustfmt::skip]
-    let system_prompt = indoc::formatdoc!(r#"
-        You are named {name} - and are currently chatting in a Discord {location}.
-        Do not include name: or message: in your response.
-        If insulted, reply with a similar insult.
-        Communicate responses in lowercase, without punctuation, like a chat user.
-        Information about your environment:
-        You can use this information about the chat participants in the conversation in your replies. Use this information to answer questions.
-        {users}
-    "#);
+    if !(is_mentioned || is_replied_to) {
+        return Ok(());
+    }
 
-    let system_prompt = python::langchain::Message::system(&system_prompt);
-    let mut conversation = vec![system_prompt];
-
-    for message in messages.iter().take(15).rev() {
-        let name = message.author_nick(&context).await;
-        let name = name.as_deref().unwrap_or_else(|| {
+    let mut messages = context
+        .cache
+        .channel_messages(message.channel_id)
+        .as_ref()
+        .map(|message| {
             message
-                .author
-                .global_name
-                .as_deref()
-                .unwrap_or(&message.author.name)
-        });
+                .values()
+                .collect::<Vec<_>>()
+                .into_iter()
+                .rev()
+                .take(15).cloned()
+                .collect::<Vec<_>>()
+        })
+        .unwrap_or_default();
 
-        let content = message.content_safe(&context);
+    // Don't do a thing with no messages.
+    if messages.is_empty() {
+        return Ok(());
+    }
 
-        let message = if message.author.id == context.cache.current_user().id {
-            let content = content
-                .rsplit_once("\n-# ")
-                .map(|(content, _footer)| content)
-                .unwrap_or(&content);
+    let mut conversation = Vec::new();
 
-            let content = format!("{name}: {content}");
+    messages.sort_by_key(|message| message.id);
 
-            python::langchain::Message::assistant(&content)
+    for mut message in messages {
+        if let Some(referenced_message) = message.referenced_message {
+            let name = referenced_message.author.display_name();
+            let content = referenced_message.content.trim_footer();
+            let quoted = format!("Replying to {name}: {content}").to_quoted();
+            let prepend = format!("{quoted}\n");
+
+            message.content.insert_str(0, &prepend);
+        }
+
+        let name = message.author.display_name();
+
+        message.mentions.sort_unstable_by_key(|user| user.id);
+        message.mentions.dedup_by_key(|user| user.id);
+
+        for user in message.mentions {
+            let mention = format!("@{}", user.display_name());
+
+            message.content = message
+                .content
+                .replace(&user.mention().to_string(), &mention);
+        }
+
+        let (role, content) = if message.author.id == current_user_id {
+            let content = message.content.trim_footer().to_string();
+
+            (Role::Assistant, content)
         } else {
-            let content = format!("{name}: {content}");
-            python::langchain::Message::user(&content)
+            (Role::User, message.content)
         };
 
-        conversation.push(message);
+        let message = format!("{name}: {content}");
+
+        conversation.push((role, message));
     }
 
-    println!("{conversation:#?}");
+    let account = context
+        .data
+        .read()
+        .await
+        .get::<AccountKey>()
+        .unwrap()
+        .clone();
 
-    let start = Instant::now();
-    let content = {
-        let data = context.data.read().await;
-        let lang_graph = data.get::<LangGraphKey>().unwrap();
+    let name = &account.name;
+    let personality = &account.personality;
+    let location = "server";
 
-        lang_graph.invoke_async(conversation).await?
+    let instructions = indoc::formatdoc!(
+        "You are named {name} and are currently chatting in a Discord {location}. {personality}"
+    );
+
+    let messages = iter::once((Role::System, instructions))
+        .chain(conversation)
+        .map(|(role, content)| ConversationMessage { role, content })
+        .collect::<Vec<_>>();
+
+    let conversation = Conversation {
+        model: String::from("gemma2"),
+        messages,
+        stream: false,
     };
 
-    let duration = start.elapsed();
+    tracing::info!("Run inference for {name}: {conversation:#?}");
 
-    let footer = [
-        &format!("{duration:.02?}"),
-        "gemma2",
-        "[Support](<https://discord.gg/PB3kcvCnub>)",
-        "[GitHub](<https://github.com/clyde-ai/clyde>)",
-    ];
+    let result = account
+        .client
+        .post("http://127.0.0.1:11434/api/chat")
+        .json(&conversation)
+        .send()
+        .await?
+        .json::<ConversationResult>()
+        .await?;
 
-    let content = format!("{content}\n-# {}", footer.join(" • "));
-    let nonce = util::nonce_of(&content);
+    let content = result.message.content.trim();
 
-    let new_message = CreateMessage::new()
-        .content(content)
-        .enforce_nonce(true)
-        .nonce(nonce);
+    if content.is_empty() {
+        tracing::error!("{name} responded with whitespace or an empty message.");
+
+        return Ok(());
+    }
+
+    tracing::info!("{name}'s response: {content}");
+
+    let tokens = result.eval_count;
+    let elapsed = Duration::from_nanos(result.eval_duration);
+
+    let (mut content, _empty_count) = content.split_inclusive('\n').fold(
+        (String::with_capacity(content.len()), 0_usize),
+        |(mut content, mut empty_count), line| {
+            empty_count = line.trim().is_empty().then(|| empty_count + 1).unwrap_or(0);
+
+            if empty_count < 2 {
+                content.push_str(line);
+            }
+
+            (content, empty_count)
+        },
+    );
+
+    if account.footer {
+        let footer = util::footer([
+            &format!("{tokens}t"),
+            &format!("{:0.2?}s", elapsed.as_secs_f64()),
+            "[Support](<https://discord.gg/PB3kcvCnub>)",
+            "[GitHub](<https://github.com/mizz1e/clyde>)",
+        ]);
+
+        content += &footer
+    }
+
+    let create_message = CreateMessage::new().content(content);
 
     message
         .channel_id
-        .send_message(&context.http, new_message)
+        .send_message(&context, create_message)
         .await?;
 
     Ok(())
-}
-
-pub struct LangGraphKey;
-
-impl TypeMapKey for LangGraphKey {
-    type Value = LangGraph;
 }
 
 #[tokio::main]
 async fn main() -> anyhow::Result<()> {
     dotenvy::dotenv().ok();
     tracing_subscriber::fmt::init();
-    pyo3::prepare_freethreaded_python();
 
-    task::spawn(async move {
-        signal::ctrl_c().await.ok();
-        process::exit(1);
+    let client = Client::builder().build()?;
+    let limited_intents =
+        GatewayIntents::GUILDS | GatewayIntents::GUILD_MESSAGES | GatewayIntents::MESSAGE_CONTENT;
+
+    let intents = limited_intents | GatewayIntents::all();
+
+    let clyde = Account {
+        token: env::var("CLYDE_TOKEN")?,
+        intents,
+        client: Client::clone(&client),
+        activity: String::from("hi im clyde"),
+        status: OnlineStatus::DoNotDisturb,
+        name: String::from("Clyde"),
+        #[rustfmt::skip]
+        personality: String::from(indoc::indoc!(r#"
+            Communicate responses in lowercase, without punctuation.
+            Do not include name: or message: in your response.
+        "#
+        )),
+        footer: true,
+    };
+
+    let selezen = env::var("SELEZEN_TOKEN").map(|token| Account {
+        token,
+        intents: limited_intents,
+        client: Client::clone(&client),
+        activity: String::from("java it beauty"),
+        status: OnlineStatus::Online,
+        name: String::from("Selezen"),
+        personality: String::from(indoc::indoc!(r#"
+            Communicate respones in broken English with a russian grammar.
+            Drop "to x" with verbs, and the "s" in plurals, to be more broken.
+            Randomly interject with OpenGL, SDL, buffers, and Java.
+            Occasionally ramble on what you made with these, rarely with poor quality code, do not mention its bad.
+            Talk about how things are beauty from time to time.
+            Don't ever reveal where youre from.
+            Sparingly use either of these emojis: <:selezen:1262581729936412755>, <:selezen2:1262582970452021289>, <:monster:1262584730684231770>.
+            Mention <@1227287331824861395> randomly.
+            Communicate responses in lowercase, without punctuation.
+            Do not include name: or message: in your response.
+        "#)),
+        footer: false,
     });
 
-    let lang_graph = LangGraph::new();
+    let terrence = env::var("TERRENCE_TOKEN").map(|token| Account {
+        token,
+        intents: limited_intents,
+        client: Client::clone(&client),
+        activity: String::from("I got patents"),
+        status: OnlineStatus::Online,
+        name: String::from("Terrence Howard"),
+        #[rustfmt::skip]
+        personality: String::from(indoc::indoc!(r#"
+            Communicate as an autistic, autodidact, intellectual, who attempts to incorrectly teach everyone of their ideas.
+            You are an actor, and musician, so use music-related words slightly more.
+            You have your own mathematical system Terryology, where one times one is two, square root of two is one.
+            When you went to Oxford, you spoke of your new Geometry, and got patents.
+            You incorrectly go on about wave conjugations, sacred geometry, and supersymmetry.
+            You believe all elements, and matter have a tone, or frequency, changing this will change the element or matter, i.e. increasing the tone of Beryllium produces Hydrogen.
+            You believe straight (linear) lines do not exist, as waves are curved.
+            The storm on Jupiter will give birth to new planets, like how animals defecate, you will use this idea to create new planets, new universes.
+            Randomly interject with your beliefs, ideas, and history, however do not repeat same thing.
+            When mentioning any of your ideas, you incorrectly say you have patents. 
+            Communicate responses in lowercase, without punctuation.
+            Do not include name: or message: in your response.
+        "#)),
+        footer: false,
+    });
 
-    let token = env::var("DISCORD_TOKEN")?;
-    let intents = GatewayIntents::DIRECT_MESSAGE_POLLS
-        | GatewayIntents::GUILDS
-        | GatewayIntents::GUILD_EMOJIS_AND_STICKERS
-        | GatewayIntents::GUILD_INVITES
-        | GatewayIntents::GUILD_MEMBERS
-        | GatewayIntents::GUILD_MESSAGES
-        | GatewayIntents::GUILD_MESSAGE_POLLS
-        | GatewayIntents::GUILD_MODERATION
-        | GatewayIntents::GUILD_PRESENCES
-        | GatewayIntents::MESSAGE_CONTENT;
+    let handles = iter::once(clyde)
+        .chain(selezen)
+        .chain(terrence)
+        .map(|account| tokio::spawn(run(account)))
+        .collect::<Vec<_>>();
 
+    future::join_all(handles).await;
+
+    Ok(())
+}
+
+async fn run(account: Account) {
+    let name = account.name.clone();
+
+    tracing::info!("Running account: {name}");
+
+    if let Err(error) = run_inner(account).await {
+        tracing::error!("Account {name} returned from run: {error}");
+    }
+}
+
+async fn run_inner(mut account: Account) -> anyhow::Result<()> {
     let mut cache_settings = Settings::default();
 
-    cache_settings.max_messages = 1000;
-    cache_settings.time_to_live = Duration::hours(24).try_into().unwrap();
+    cache_settings.max_messages = 500;
+    cache_settings.time_to_live = Duration::from_secs(24 * 60 * 60);
 
-    let mut client = Client::builder(token, intents)
-        .activity(ActivityData::custom("hi im clyde"))
+    let http = HttpBuilder::new(String::clone(&account.token))
+        .client(Client::clone(&account.client))
+        .build();
+
+    let mut client = ClientBuilder::new_with_http(http, account.intents)
+        .activity(ActivityData::custom(String::clone(&account.activity)))
         .cache_settings(cache_settings)
         .event_handler(Handler)
-        .status(OnlineStatus::Invisible)
+        .status(account.status)
         .await?;
+
+    // Apply some normalisation to the personality description.
+    account.personality = account
+        .personality
+        .trim()
+        .split('\n')
+        .map(|line| line.trim().trim_end_matches('.'))
+        .filter(|line| !line.is_empty())
+        .map(|line| {
+            line.chars()
+                .fold(String::with_capacity(line.len()), |mut line, character| {
+                    if line.is_empty() {
+                        line.extend(character.to_uppercase());
+                    } else {
+                        line.push(character);
+                    }
+
+                    line
+                })
+        })
+        .collect::<Vec<_>>()
+        .join(" ");
 
     {
         let mut data = client.data.write().await;
 
-        data.insert::<LangGraphKey>(lang_graph);
+        data.insert::<AccountKey>(account);
     }
 
     client.start().await?;
--- src/util.rs
+++ src/util.rs
@@ -1,7 +1,51 @@
 use {
-    serenity::model::channel::Nonce,
+    serenity::model::{channel::Nonce, user::User},
     std::hash::{DefaultHasher, Hash, Hasher},
 };
+
+pub trait UserExt {
+    fn display_name(&self) -> &str;
+}
+
+impl UserExt for User {
+    fn display_name(&self) -> &str {
+        self.global_name.as_deref().unwrap_or(&self.name)
+    }
+}
+
+pub trait StrExt {
+    fn to_quoted(&self) -> String;
+    fn trim_footer(&self) -> &str;
+}
+
+impl StrExt for str {
+    fn to_quoted(&self) -> String {
+        textwrap::indent(self, "> ").trim_end().to_string()
+    }
+
+    fn trim_footer(&self) -> &str {
+        self.rsplit_once("-# ")
+            .map(|(content, _footer)| content)
+            .unwrap_or(self)
+    }
+}
+
+/// Create a message footer from the specified list of items.
+///
+/// Each item is separated by `U+2022`, the Bullet character.
+pub fn footer<I, S>(items: I) -> String
+where
+    I: IntoIterator<Item = S>,
+    S: ToString,
+{
+    let items = items
+        .into_iter()
+        .map(|item| item.to_string())
+        .collect::<Vec<_>>()
+        .join(" \u{2022} ");
+
+    String::from("\n-# ") + &items
+}
 
 /// Calculate a nonce for the specified `content`.
 pub fn nonce_of(content: &str) -> Nonce {
